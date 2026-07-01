12ba236934689f325b623935b8ae3faf50a30645
Saravanan <saravanan642@gmail.com>
2026-07-01 17:52:08 +0530

    update

--- Controller/usercode.js
+++ Controller/usercode.js
@@ -1,36 +1,38 @@
 const Modeluser = require("../Models/user")
 
-const createrUser = async(req ,res) =>{
+const createrUser = async (req, res) => {
     try{
         const{ name, email, age, password, phone, address, gender,isActive,dob,profileImage,skills,marks}=req.body
 
         if(!name || !email || !password || !age || !phone || !isActive || !gender || !isActive || !profileImage || !skills || !marks){
-            return res.josn({success : false , message :  "All filed are required"})
+            return res.json({success : false , message :  "All filed are required"})
         }
 
         const isExiting = await Modeluser.findOne({email})
         if(isExiting) {
-            return res.josn({success : false , message : "Already account exits please try again later"})
+            return res.json({success : false , message : "Already account exits please try again later"})
         }
 
         const phoneExixtig = await Modeluser.findOne({phone})
-            if(phoneExixtig) {
-                return res.josn(({success : false , message : "Already number exists plese try again later"}))
-            }
-        
-            const newUser = new Modeluser ({
-                name, email, age, password, phone, address, gender,isActive,dob,profileImage,skills,marks
-            })
+        if(phoneExixtig) {
+            return res.json({success : false , message : "Already number exists plese try again later"})
+        }
 
-            const save = await newUser.save()
+        const newUser = new Modeluser({
+            name, email, age, password, phone, address, gender,isActive,dob,profileImage,skills,marks
+        })
 
-            if(!save){
-                return res.josn({success :false, message :"Failed to create please try again later" })
-            }
-         return res.json({success : true , message : "Account cretaed successfully" })
+        const save = await newUser.save()
+
+        if(!save){
+            return res.json({success :false, message :"Failed to create please try again later" })
+        }
+
+        return res.json({success : true , message : "Account cretaed successfully" })
 
     }catch(err){
         console.log("Error in create user",err.message)
     }
 }
+
 module.exports = createrUser;
